2eaa4e3a5236efe73334d01b984a29c3fc8aaf2f
richarn <rnezferreira9@gmail.com>
2023-02-02 18:39:18 -0300

    Initializing project

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,15 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+
+        {/* titulo */}
+        <h1>GifExpertApp</h1>
+
+        {/* Input */}
+
+        {/* Listado de Glif */}
+            {/* Gif Item */}
+    </>
+  )
+}
