a942d2dd44dc7bf9f948a16a12ddebe17d05ce6c
richarn <rnezferreira9@gmail.com>
2023-02-16 17:27:23 -0300

    desestructuracion y agregado de nuevas categoria en usesState

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,16 +5,25 @@ export const GifExpertApp = () => {
 
   
   const [categories, setcategories] = useState(['One punch', 'other thing']);
-  console.log(categories);
+
+  const onAddCategory = () => {
+    
+    setcategories([ ...categories, 'Hello' ]);
+
+    // console.log('Hello');
+  }
+
+
   return (
     <>
-
         {/* titulo */}
         <h1>GifExpertApp</h1>
 
         {/* Input */}
 
         {/* Listado de Glif */}
+
+        <button onClick={ onAddCategory }>Agregar</button>
         <ol>
             {/* recorre el hook categories */}                
             { categories.map( category => {
@@ -22,6 +31,7 @@ export const GifExpertApp = () => {
                 return <li key={ category }>{ category }</li>
                 }) 
             }
+            
         </ol>
                     
             {/* Gif Item */}
